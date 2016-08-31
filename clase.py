b6ebb20a69106cc216d1f2b4412f8f2827d308c1
Virgilio Paniagua <pino_2586@hotmail.com>
2016-08-31 14:52:38 -0600

    Update clase.py

--- clase.py
+++ clase.py
@@ -5,4 +5,8 @@ class numeros:
     def division(self,c,d):
         return c/d
     
-        
+obj = numeros ()
+
+print "Metodo Multiplicar:", obj.multiplicar(5,5)
+
+print "Metodos Division:", obj.division(100,5)
