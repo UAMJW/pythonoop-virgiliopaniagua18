b08289850738f226da9f851e82de4dfa272911d2
Virgilio Paniagua <pino_2586@hotmail.com>
2016-08-31 14:27:33 -0600

    Python - Definir Clase

---
+++ clase.py
@@ -0,0 +1,8 @@
+class numeros:
+    def multiplicar(self,a,b):
+        return a*b
+
+    def division(self,c,d):
+        return c/d
+    
+        
---
+++ main.py
@@ -0,0 +1,7 @@
+from clase import numeros
+
+obj = numeros ()
+
+print "Metodo Multiplicar:", obj.multiplicar(5,5)
+
+print "Metodos Division:", obj.division(100,5)
